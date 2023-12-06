c2b1664b5431fa5bb841ee50fdac27f523886c91
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-12-06 16:37:45 +0000

    use react-router-dom to create routes and protect using AuthContext, ternaries and Outlet

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,9 +1,10 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+  useEffect(() => console.log(auth), [auth]);
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthContextProvider } from "./contexts/AuthContext.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        {/* <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes> */}
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
